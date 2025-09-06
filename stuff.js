98ccc72077fb5228dc7e146c5069c61df53751ac
ezkathhhs <matiazloyola@gmail.com>
2025-09-06 01:08:55 -0400

    Navbar, Subnavbar, footer, archivos

---
+++ stuff.js
@@ -0,0 +1,27 @@
+function redirectToHome() {
+    window.location.href = "index.html";
+}
+
+function redirectToLogin() {
+    window.location.href = "login.html";
+}
+
+function redirectToRegister() {
+    window.location.href = "registro.html";
+}
+
+function redirectToProducts() {
+    window.location.href = "productos.html";
+}
+
+function redirectToAbout() {
+    window.location.href = "nosotros.html";
+}
+
+function redirectToBlog() {
+    window.location.href = "blogs.html";
+}
+
+function redirectToContact() {
+    window.location.href = "contacto.html";
+}
