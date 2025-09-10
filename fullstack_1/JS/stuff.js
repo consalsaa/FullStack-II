60d6fdbe135271ad6ae3e17f85d3005de748c34a
ezkathhhs <matiazloyola@gmail.com>
2025-09-10 04:06:48 -0300

    Productos y otras paginas

---
+++ fullstack_1/JS/stuff.js
@@ -0,0 +1,105 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function redirectToAdmin() {
+    window.location.href = "admin.html";
+}
+function redirectToBlog() {
+    window.location.href = "blogs.html";
+}
+function redirectToCarrito() {
+    window.location.href = "carrito.html";
+}
+function redirectToContact() {
+    window.location.href = "contacto.html";
+}
+function redirectToHome() {
+    window.location.href = "index.html";
+}
+function redirectToLogin() {
+    window.location.href = "login.html";
+}
+function redirectToAbout() {
+    window.location.href = "nosotros.html";
+}
+function redirectToProducts() {
+    window.location.href = "productos.html";
+}
+function redirectToRegister() {
+    window.location.href = "registro.html";
+}
+
+//Blogs
+function redirectToBlog1() {
+    window.location.href = "blog1.html";
+}
+function redirectToBlog2() {
+    window.location.href = "blog2.html";
+}
+
+//Productos
+function redirectToProduct1() {
+    window.location.href = "smiski.html";
+}
+function redirectToProduct2() {
+    window.location.href = "pompompurin.html";
+}
+function redirectToProduct3() {
+    window.location.href = "mk.html";
+}
+function redirectToProduct4() {
+    window.location.href = "cinnamoroll.html";
+}
+function redirectToProduct5() {
+    window.location.href = "sanrio.html";
+}
+function redirectToProduct6() {
+    window.location.href = "snoopy.html";
+}
